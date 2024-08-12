4e5fa3c78a07f74f046259755431e008ec766c3a
kareem <1998kareem@gmail.com>
2024-08-12 11:48:49 +0300

    test commit

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Test() {
-  return <div className="fixed z-50 w-full bg-red-400">Test</div>;
+  return <div className="fixed z-50 w-full bg-red-500">Test</div>;
 }
 
 export default Test;
