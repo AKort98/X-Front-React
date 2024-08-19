5d4a9724f0a32c71285d150fa74682c275df8aa2
kareem <1998kareem@gmail.com>
2024-08-19 10:49:15 +0300

    added following and follower count to profiles

--- src/components/Profile/ProfileDetails.jsx
+++ src/components/Profile/ProfileDetails.jsx
@@ -8,6 +8,18 @@ function ProfileDetails({ data }) {
       </span>
       <span className="text-lg text-[#77777779]">@{data.user.username}</span>
       <p className="mt-4 text-white">{data.user.description}</p>
+      <div className="flex gap-4">
+        <div className="mt-2 flex gap-1">
+          <span className="font-semibold text-white">
+            {data.followingCount}
+          </span>
+          <span className="text-[#77777779]">Following</span>
+        </div>
+        <div className="mt-2 flex gap-1">
+          <span className="font-semibold text-white">{data.followerCount}</span>
+          <span className="text-[#77777779]">Followers</span>
+        </div>
+      </div>
     </div>
   );
 }
